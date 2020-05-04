d0fe670cf2a0ab8470d0ddb1523cc2a4426aa2a2
Nikolay Yasinskiy <jasny_jasen@192.168.0.19>
2020-05-04 14:26:38 +0300

    all

---
+++ sneg2.py
@@ -0,0 +1,6 @@
+# Разработка алгоритма SNEG-2
+from pcraster import *
+import numpy
+from osgeo import gdal
+import subprocess
+
--- test_pcraster.py
+++ test_pcraster.py
@@ -40,3 +40,42 @@ pcraster.aguila('accum.map')
 smmd='map2asc -a accum.map accum.asc'
 result=subprocess.run(smmd,shell=True,capture_output=True)
 #print(result.stderr)
+
+#%%gdal to PCR
+path='/home/jasny_jasen/Документы/PROJECTS/SNEG2/data/'
+testfile='pr_day_ACCESS1-0_historical_r1i1p1_1961-2005box.nc'
+prjfile='pr_Amon_ACCESS1-0_rcp85_r1i1p1_1961_90box_remapmulcyearsum.nc'
+sneg='19801009_northern_hemisphere_swe_0.25grid.nc'
+test=gdal.Open(path+testfile)
+prj=gdal.Open(path+prjfile)
+#gdal.Info(path+sneg)
+#%%
+from osgeo import gdal, gdalconst
+
+path='/home/jasny_jasen/Документы/PROJECTS/SNEG2/data/'
+testfile='pr_day_ACCESS1-0_historical_r1i1p1_1961-2005box.nc'
+prjfile='pr_Amon_ACCESS1-0_rcp85_r1i1p1_1961_90box_remapmulcyearsum.nc'
+sneg='19801009_northern_hemisphere_swe_0.25grid.nc'
+
+# Source
+src = gdal.Open(path+sneg, gdalconst.GA_ReadOnly)
+src_proj = src.GetProjection()
+src_geotrans = src.GetGeoTransform()
+
+# We want a section of source that matches this:
+match_ds = gdal.Open(path+prjfile, gdalconst.GA_ReadOnly)
+match_proj = match_ds.GetProjection()
+match_geotrans = match_ds.GetGeoTransform()
+wide = match_ds.RasterXSize
+high = match_ds.RasterYSize
+
+# Output / destination
+dst_filename = path+'precout2.tif'
+dst = gdal.GetDriverByName('GTiff').Create(dst_filename, wide, high, 1, gdalconst.GDT_Float32)
+dst.SetGeoTransform( match_geotrans )
+dst.SetProjection( match_proj)
+
+# Do the work
+gdal.ReprojectImage(src, dst, src_proj, match_proj, gdalconst.GRA_Bilinear)
+
+del dst # Flush
